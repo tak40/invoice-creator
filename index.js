284b6de446dc9468eb813a95ea0e126295c3038c
Tak <takeshiseodev@gmail.com>
2023-10-12 22:44:47 -0400

    Modal Pop-up Enhancements and Refinements

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Grab the DOM elements
-
 const taskInput = document.getElementById('task')
 const priceInput = document.getElementById('price')
 const quantityInput = document.getElementById('quantity')
@@ -11,10 +10,26 @@ const taskTotalsDiv = document.getElementById('task-totals')
 const totalAmountDiv = document.getElementById('total-amount')
 const sendInvoiceButton = document.getElementById('send-invoice-btn')
 
+// Grab the modal DOM elements
+const invoiceModal = document.getElementById('invoice-modal')
+const submitModalBtn = document.getElementById('submit-modal-btn')
+const cancelModalBtn = document.getElementById('cancel-modal-btn')
+const confirmationModal = document.getElementById('confirmation-modal')
+const closeConfirmModalBtn = document.getElementById('close-confirm-modal-btn')
+
+
 // Event Listeners
 addTaskButton.addEventListener('click', addTask)
 tasksContainerDiv.addEventListener('click', handleTaskEvents)
-sendInvoiceButton.addEventListener('click', sendInvoice)
+sendInvoiceButton.addEventListener('click', () => invoiceModal.showModal())
+
+// Modal Event Listeners
+submitModalBtn.addEventListener('click', processInvoice)
+cancelModalBtn.addEventListener('click', () => invoiceModal.close())
+closeConfirmModalBtn.addEventListener('click', () => confirmationModal.close())
+document.addEventListener('mousedown', closeModalOnMouseDownOutside)
+
+
 
 function handleTaskEvents(event) {
     // Identify the closest parent task section from the clicked element
@@ -30,17 +45,17 @@ function handleTaskEvents(event) {
     // Check if the clicked element is the "remove" button
     if (event.target.classList.contains('remove-btn')) {
         // If so, call the removeTask function to delete the task
-        removeTask(targetElement);
+        removeTask(targetElement)
     } 
     // Check if the clicked element is the "plus" button
     else if (targetElement.classList.contains('plus-btn')) {
         // If so, call the updateQuantity function to increase the quantity by 1
-        updateQuantity(taskSection, 1);
+        updateQuantity(taskSection, 1)
     } 
     // Check if the clicked element is the "minus" button
     else if (targetElement.classList.contains('minus-btn')) {
         // If so, call the updateQuantity function to decrease the quantity by 1
-        updateQuantity(taskSection, -1);
+        updateQuantity(taskSection, -1)
     }
 }
 
@@ -94,11 +109,11 @@ function updateQuantity(taskElement, delta) {
     const itemPrice = Number(totalElement.getAttribute('data-price'))
 
     // Calculate the new quantity
-    let newQuantity = Number(quantityElement.textContent) + delta;
+    let newQuantity = Number(quantityElement.textContent) + delta
 
     // If the new quantity is below 0, remove the task and exit the function
     if (newQuantity <= 0) {
-        removeTask(taskElement);
+        removeTask(taskElement)
         return;
     }
 
@@ -106,13 +121,13 @@ function updateQuantity(taskElement, delta) {
     quantityElement.textContent = newQuantity
 
     // Get the minus button
-    const minusBtn = taskSection.querySelector('.minus-btn');
+    const minusBtn = taskSection.querySelector('.minus-btn')
 
     // Check if new quantity is 1 and update the button content accordingly
     if (newQuantity === 1) {
-        minusBtn.innerHTML = '<i class="fas fa-trash"></i>';
+        minusBtn.innerHTML = '<i class="fas fa-trash"></i>'
     } else {
-        minusBtn.textContent = '-';
+        minusBtn.textContent = '-'
     }
 
     // Update the total for this item
@@ -122,32 +137,79 @@ function updateQuantity(taskElement, delta) {
     updateTotalAmount()
 }
 
-function sendInvoice() {
-    // Remove all tasks from the task list
-    while(tasksContainerDiv.firstChild) {
-        tasksContainerDiv.removeChild(tasksContainerDiv.firstChild);
-    }
-
-    // Update the total amount to reflect the empty task list
-    updateTotalAmount();
-}
-
 // Utility Functions
 function updateTotalAmount() {
     // Variable to hold the sum of all the 'total' elements
-    let totalAmount = 0;
+    let totalAmount = 0
 
     // Grab all the 'total' elements
-    const totalElements = document.querySelectorAll('.total');
+    const totalElements = document.querySelectorAll('.total')
 
     // Loop through each 'total' element and add its value to the totalAmount variable
     totalElements.forEach(function (totalElement) {
         // Remove the dollar sign before converting to a number
-        const numericValue = Number(totalElement.textContent.replace('$', ''));
-        totalAmount += numericValue;
+        const numericValue = Number(totalElement.textContent.replace('$', ''))
+        totalAmount += numericValue
     });
 
     // Update the total amount with the dollar sign
-    totalAmountDiv.textContent = `$${totalAmount}`;
+    totalAmountDiv.textContent = `$${totalAmount}`
 }
+
+// Modal Functions
+
+function clearAllTasks() {
+    // Remove all tasks from the task list
+    while(tasksContainerDiv.firstChild) {
+        tasksContainerDiv.removeChild(tasksContainerDiv.firstChild)
+    }
+    // Update the total amount to reflect the empty task list
+    updateTotalAmount()
+}
+
+function processInvoice() {
+    // Here you'd normally process the invoice, but for this example:
+    clearAllTasks()
+    invoiceModal.close()
+    // Display the confirmation message:
+    confirmationModal.showModal()
+}
+
+function closeModalOnMouseDownOutside(event) {
+    console.log("Mouse down detected");
+
+    if (invoiceModal.hasAttribute('open')) {
+        console.log("Invoice modal is open");
+        if (!invoiceModal.contains(event.target)) {
+            console.log("Click was outside of invoice modal");
+            invoiceModal.close();
+        }
+    }
+
+    if (confirmationModal.hasAttribute('open')) {
+        console.log("Confirmation modal is open");
+        if (!confirmationModal.contains(event.target)) {
+            console.log("Click was outside of confirmation modal");
+            confirmationModal.close();
+        }
+    }
+}
+
+function closeDialogOnClickOutside(dialog, cancelBtnId) {
+    dialog.addEventListener("click", e => {
+        const dialogDimensions = dialog.getBoundingClientRect()
+        if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom ||
+            e.target.id === cancelBtnId
+        ) {
+            dialog.close()
+        }
+    })
+}
+
+closeDialogOnClickOutside(invoiceModal, "cancel-modal-btn")
+closeDialogOnClickOutside(confirmationModal, "close-confirm-modal-btn")
 
